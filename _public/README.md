e5a5b51a6c176cf6804473ff87e6bd925e4c69c5
Simon Eramo <simonramo@gmail.com>
2014-11-20 13:54:34 +1100

    restored

---
+++ _public/README.md
@@ -0,0 +1,3 @@
+# Core App
+
+the main files of your application
---
+++ app/assets/scripts/vendor/jquery.js
@@ -0,0 +1,7 @@
+// Jquery - bower_components/jquery/dist/
+
+ $(document).ready(function(){
+        $('.o-burger').on('click',function(){
+          $(this).toggleClass('opened');
+        });
+      });
