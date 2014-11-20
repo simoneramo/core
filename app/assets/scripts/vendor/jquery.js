dc668b2a256a1b248d66008c6fbe68df03feaea7
Simon Eramo <simonramo@gmail.com>
2014-11-20 12:53:14 +1100

    Update 2014-11-20T01:52:25.162Z

--- app/assets/scripts/vendor/jquery.js
+++
@@ -1,7 +0,0 @@
-// Jquery - bower_components/jquery/dist/
-
- $(document).ready(function(){
-        $('.o-burger').on('click',function(){
-          $(this).toggleClass('opened');
-        });
-      });
