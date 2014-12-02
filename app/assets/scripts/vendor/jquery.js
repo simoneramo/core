4f22b29a2a9df571253908abdd9508645997d14e
Simon Eramo <simonramo@gmail.com>
2014-12-02 21:48:08 +1100

    styleguide updates ect

--- app/assets/scripts/vendor/jquery.js
+++ app/assets/scripts/vendor/jquery.js
@@ -1,7 +1,1 @@
 // Jquery - bower_components/jquery/dist/
-
- $(document).ready(function(){
-        $('.o-burger').on('click',function(){
-          $(this).toggleClass('--opened');
-        });
-      });
