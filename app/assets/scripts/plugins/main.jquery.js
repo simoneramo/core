cced043cd06d4e720e1c06883f76e7fc36e67f73
Simon Eramo <simonramo@gmail.com>
2014-12-10 22:02:31 +1100

    house keeping

--- app/assets/scripts/plugins/main.jquery.js
+++ app/assets/scripts/plugins/main.jquery.js
@@ -6,5 +6,10 @@ $(document).ready(function(){
   $('.js-z-index > div').on('click',function(){
     $(this).toggleClass('+u-z-5');
   });
+
+  $('.styleguide-utilities.position .\\+u-p-fixed .js-close').on('click',function(){
+    $('.\\+u-p-fixed').hide();
+  });
+
 });
 
